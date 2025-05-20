d4ced299fb272323599cb5f80cc79af90d4f9ab4
Renssus <74822300+Renssus@users.noreply.github.com>
2025-05-20 12:15:56 +0200

    yessir

--- resources/views/layouts/error.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($errors->any())
-    <div class="bg-red-500 text-white p-4 rounded my-2">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,24 @@
 <?php
 
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\CarController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
 
-    Route::resource('cars', CarController::class)->except(['show']);
-    Route::get('/cars/create', [CarController::class, 'showAddCarForm'])->name('cars.show');
-    Route::post('/cars/store', [CarController::class, 'store'])->name('cars.store');
-
-    Route::post('/cars/get-rdw-data', [CarController::class, 'getCarDataFromRdw'])->name('cars.get-rdw-data');
-
-    Route::middleware('auth')->get('/my-cars', [CarController::class, 'index'])->name('cars.index');
-
-    Route::middleware('auth')->get('/cars/{car}/edit', [CarController::class, 'edit'])->name('cars.edit');
-
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 });
 
 require __DIR__.'/auth.php';
